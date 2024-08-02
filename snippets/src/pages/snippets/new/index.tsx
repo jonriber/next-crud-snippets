6269163101b2b220a37a4e95209ec4b3c2360527
Jonatas Ribeiro <jonatas.m.ribeiro@noesis.pt>
2024-08-02 10:28:58 +0100

    creating a rootLayout for all of my pages & fixing errors

--- snippets/src/pages/snippets/new/index.tsx
+++ snippets/src/pages/snippets/new/index.tsx
@@ -1,7 +1,31 @@
 export default function NewSnippet() {
   return (
-    <div>
-      New Snippet Page
-    </div>
+    <form>
+        <h3 className="font-bold m-3">Create a Snippet</h3>
+        <div className="flex flex-col gap-4">
+            <div className="flex gap-4">
+                <label className="w-12" htmlFor="title">Title</label>
+                <input 
+                    className="border rounded p-2 w-full"
+                    type="text" 
+                    id="title" 
+                    name="title" 
+                    />
+            </div>
+
+            <div className="flex gap-4">
+                <label className="w-12" htmlFor="code">Code</label>
+                <textarea 
+                    className="border rounded p-2 w-full"
+                    id="code" 
+                    name="code" 
+                    />
+            </div>
+
+            <button type="submit" className="rounded p-2 bg-blue-200">
+                Create
+            </button>
+        </div>
+    </form>
   );
 }
