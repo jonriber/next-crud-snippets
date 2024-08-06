400f7dcef67dff946b5989377e6b65e70238931d
Jonatas Ribeiro <jonatas.m.ribeiro@noesis.pt>
2024-08-06 17:59:39 +0100

    editing page & linking to editing page

---
+++ snippets/src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,14 @@
+interface SnippetEditPageProps {
+    params: {
+        id: string;
+    }
+}
+
+export default function SnippetEditPage(props: SnippetEditPageProps){
+    const id = parseInt(props.params.id);
+    return (
+        <>
+
+        </>
+    )
+}
--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 interface SnippetProps {
     params: {
         id: string;
@@ -20,11 +21,17 @@ export default async function Snippet(props: SnippetProps) {
 
     return (
         <>
-            <div>
-                <h2>Snippet {snippet.id}</h2>
-                <h3>{snippet.title}</h3>
-                <p>{snippet.code}</p>
+            <div className="flex m-4 justify-between items-center">
+                <h1 className="text-xl font-bold">{snippet.title}</h1>
+
+                <div className="flex gap-4">
+                    <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
+                    <button className="p-2 border rounded">Delete</button>
+                </div>
             </div>
+            <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+                <code>{snippet.code}</code>
+            </pre>
         </>
         
     );
