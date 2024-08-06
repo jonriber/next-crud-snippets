c944d225738c5e5d805e4414348e7d25c2e8d653
Jonatas Ribeiro <jonatas.m.ribeiro@noesis.pt>
2024-08-06 16:08:28 +0100

    create snippet page & layout update

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,20 +1,31 @@
 export default function NewSnippetPage() {
   return (
     <form>
-        <h3>Create a Snippet</h3>
-        <label>
-            Title
-            <input type="text" name="title" />
-        </label>
-        <label>
-            Description
-            <input type="text" name="description" />
-        </label>
-        <label>
-            Code
-            <textarea name="code" />
-        </label>
-        <button type="submit">Create Snippet</button>
+        <h3 className="font-bold m-3">Create a Snippet</h3>
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-4">
+            <label className="w-12" htmlFor="title">
+              Title
+            </label>
+            <input 
+              id="title"
+              className="border rounded p-2 w-full" 
+              name="title" 
+              />
+          </div>
+
+          <div className="flex gap-4">
+            <label className="w-12" htmlFor="code">
+              Code
+            </label>
+            <textarea 
+              id="code"
+              className="border rounded p-2 w-full" 
+              name="code" 
+              />
+          </div>
+          <button type="submit" className="rounded p-2 bg-blue-200">Create Snippet</button>
+        </div>
     </form>
   );
 }
